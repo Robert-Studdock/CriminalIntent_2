eaf6c5e34854f4cad4e2006a1047bf81e879795b
Robert <robertstuddock@Hotmail.com>
2017-09-11 17:15:56 +1000

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,68 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Robert on 17/08/2017.
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+    private String mSuspect;
+
+
+    public Crime() {
+        this(UUID.randomUUID());
+       // mId = UUID.randomUUID();
+       // mDate = new Date();
+
+    }
+
+    public Crime(UUID id) {
+        mId = id;
+        mDate = new Date();
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public UUID getId() {return mId; }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public String getSuspect() {
+        return mSuspect;
+    }
+
+    public  void  setSuspect(String suspect) {
+        mSuspect = suspect;
+    }
+
+    public  String getPhotoFilename() {
+        return "IMG_" + getId().toString() + ".jpg";
+    }
+}
+
